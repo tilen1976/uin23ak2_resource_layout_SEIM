43abc0be134ce8686c97b7785bbf6bc408a41f2e
Cecilie Seim <totilie8@gmail.com>
2023-01-17 13:48:10 +0100

    Moved logic into seperate js file

---
+++ getResources.js
@@ -0,0 +1,42 @@
+//variables 
+let navtitle = "";
+
+let listElement = "";
+
+//Loop through resources and store title in variable
+resources.map(
+    (item) => { navtitle += `<a href="#${item.category}"  id="${item.category}"  onclick="myFunction(this)">${item.category}</a>`}
+
+)
+
+//Display variable content
+document.getElementById("title").innerHTML = navtitle;
+
+//function for button to get all info from object
+function myFunction(e){
+    
+    document.getElementById("headline").innerHTML = e.id;
+
+
+    let text = "";
+    let link = "";
+
+   resources.map(
+        (item) => {
+        if(item.category === e.id){
+            text += `${item.text}`,
+            item.sources.map(
+                (item) => {
+                  link += `<li><a href="${item.url}">${item.title}</a></li>` 
+                }
+               
+            )
+        };
+            document.getElementById("text").innerHTML = text;
+            document.getElementById("listSource").innerHTML = link;
+
+        }
+   );
+
+}
+
