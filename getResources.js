141588e7a1c4adea4857416cfd575335e47fa4b8
Cecilie Seim <totilie8@gmail.com>
2023-01-23 17:40:11 +0100

    Ordnet endring av stil på valgt a i javascript

--- getResources.js
+++ getResources.js
@@ -1,26 +1,44 @@
 
-//variable for nav titles
+//variable for titler i nav
 let navtitle = "";
 
-//Loop through resources and store title in variables. Define id and make a elements perform onclick function.
+//hente ut tittel og legge i variabel. trim() for å fjerne whitespace
+//Definere en id og legge til function på onClick
 resources.map(
-    (item) => { navtitle += `<a href="#${item.category.trim()}"  id="${item.category}"  onClick="myFunction(this)">${item.category.trim()}</a>`}
+    (item) => { navtitle += `<a href="#${item.category.trim()}"  id="${item.category}"  class="defaultStyle" onClick="myFunction(this)">${item.category.trim()}</a>`}
 
 )
 
-//Display variable content
+
+
+//noe onload?
+
+//Vise innhold i dokument
 document.querySelector("#title").innerHTML = navtitle;
 
-//function for retrieving data from category
+//Funksjon som henter ut info om hver kategori
 function myFunction(e){
     
     document.querySelector("#headline").innerHTML = e.id;
+
     
     let text = "";
     let link = "";
 
    resources.map(
         (item) => {
+
+            //kilde student Stine
+            let elements = document.querySelectorAll(".defaultStyle");
+            for(let i = 0; i < elements.length; i++){
+                elements[i].classList.remove("styleChange");
+            }
+             
+        
+            let element = document.getElementById(e.id)
+        
+            element.classList.add("styleChange")
+           
         if(item.category === e.id){
             text += `${item.text}`,
             item.sources.map(
@@ -33,7 +51,10 @@ function myFunction(e){
     
             document.querySelector("#text").innerHTML = text;
             document.querySelector("#listSource").innerHTML = link;
+
+
         }
    );
+  
    
 }
