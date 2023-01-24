98974f6119356b088ad68d983f8b5f1de41ac850
Cecilie Seim <totilie8@gmail.com>
2023-01-24 08:48:00 +0100

    Endret linjeoppsett av funksjon for lesbarhet

--- getResources.js
+++ getResources.js
@@ -3,11 +3,15 @@ let navtitle = "";
 /*hente ut tittel og legge i variabel.
   Definere en id og legge til function på onClick*/
 resources.map(
-    (item) => { navtitle += `<a href="#${item.category}"  id="${item.category}"  class="defaultStyle" onClick="myFunction(this)">${item.category}</a>`}
-
-)
+    (item) => { 
+        navtitle += `<a href="#${item.category}"  
+        id="${item.category}"  class="defaultStyle" 
+        onClick="myFunction(this)">${item.category}</a>`
+    }
+);
 
 document.querySelector("#title").innerHTML = navtitle;
+
 
 /*Funksjon som henter ut info om hver kategori
   Endrer style på anchor tag
@@ -16,8 +20,7 @@ document.querySelector("#title").innerHTML = navtitle;
 function myFunction(e){
     
     document.querySelector("#headline").innerHTML = e.id;
-
-    
+ 
     let text = "";
     let link = "";
 
@@ -25,17 +28,17 @@ function myFunction(e){
         (item) => {
 
             /*De tre neste kodelinjene har jeg funnet hjelp hos gruppa mi, A-team, 
-            i tilfelle det bør være kilde på koden*/
-            let elements = document.querySelectorAll(".defaultStyle");
+            og kan derfor ligne på de andres løsning akkurat her*/
+           let elements = document.querySelectorAll(".defaultStyle");
 
             for(let i = 0; i < elements.length; i++){
                 elements[i].classList.remove("styleChange");
             }
              
         
-            let element = document.getElementById(e.id)
+            let element = document.getElementById(e.id);
             
-            element.classList.add("styleChange")
+            element.classList.add("styleChange");
   
             if(item.category === e.id){
 
@@ -52,5 +55,6 @@ function myFunction(e){
             document.querySelector("#listSource").innerHTML = link;
             document.querySelector("main").classList.add("showMain");
         }
+        
    ); 
 }
